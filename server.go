23d66d2ecaabd9a3f4368fced68ad129986f48da
Kyrill Zorin <zorink@zoringroup.com>
2015-10-19 00:45:09 +0100

    Added Thread (goroutine) pooling. Added configuration file. Updated start script to add port number and config.

--- server.go
+++ server.go
@@ -5,11 +5,88 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
-var IP = getIP()
-var PORT = getPort()
+var (
+	IP        = getIP()
+	PORT      = getPort()
+	MaxWorker = getMaxWorkers()
+	MaxQueue  = getMaxQueue()
+)
+var ConnQueue chan net.Conn = make(chan net.Conn, MaxQueue)
+
+type Worker struct {
+	WorkerPool  chan chan net.Conn
+	ConnChannel chan net.Conn
+	quit        chan bool
+}
+
+func NewWorker(workerPool chan chan net.Conn) Worker {
+	return Worker{
+		WorkerPool:  workerPool,
+		ConnChannel: make(chan net.Conn),
+		quit:        make(chan bool)}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			w.WorkerPool <- w.ConnChannel
+
+			select {
+			case conn := <-w.ConnChannel:
+				handleRequest(conn)
+
+			case <-w.quit:
+				return
+			}
+		}
+	}()
+}
+
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
+
+type Supervisor struct {
+	WorkerPool chan chan net.Conn
+	MaxWorkers int
+}
+
+func NewSupervisor(maxWorkers int) *Supervisor {
+	pool := make(chan chan net.Conn, maxWorkers)
+	return &Supervisor{WorkerPool: pool, MaxWorkers: maxWorkers}
+}
+
+func (s *Supervisor) Run() {
+	for i := 0; i < s.MaxWorkers; i++ {
+		worker := NewWorker(s.WorkerPool)
+		worker.Start()
+	}
+
+	go s.dispatch()
+}
+
+func (s *Supervisor) dispatch() {
+	for {
+		select {
+		case conn := <-ConnQueue:
+			go func(conn net.Conn) {
+				connChannel := <-s.WorkerPool
+				connChannel <- conn
+			}(conn)
+		}
+	}
+}
+
+func init() {
+	supervisor := NewSupervisor(MaxWorker)
+	supervisor.Run()
+}
 
 func main() {
 	listen, err := net.Listen("tcp", IP+":"+PORT)
@@ -19,13 +96,15 @@ func main() {
 	}
 	defer listen.Close()
 	fmt.Println("Listening on " + IP + ":" + PORT)
+	fmt.Println("Max Workers:", MaxWorker)
+	fmt.Println("Max Queued Connections:", MaxQueue)
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
-		go handleRequest(conn)
+		ConnQueue <- conn
 	}
 }
 
@@ -73,4 +152,22 @@ func getPort() string {
 		return e
 	}
 	return "8080"
+}
+
+func getMaxWorkers() int {
+	e := os.Getenv("CS4032_LAB_2_MAX_WORKERS")
+	if len(e) > 0 {
+		i, _ := strconv.Atoi(e)
+		return i
+	}
+	return 15000
+}
+
+func getMaxQueue() int {
+	e := os.Getenv("CS4032_LAB_2_MAX_QUEUE")
+	if len(e) > 0 {
+		i, _ := strconv.Atoi(e)
+		return i
+	}
+	return 20000
 }
